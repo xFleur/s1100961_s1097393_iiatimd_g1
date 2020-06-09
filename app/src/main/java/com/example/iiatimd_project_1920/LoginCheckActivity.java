346922fdd8832db159df096df90e370fb35785b7
xFleur <mels1999@live.nl>
2020-06-09 14:55:20 +0200

    login screen toegevoegd (werkend hardcoded) + git check

---
+++ app/src/main/java/com/example/iiatimd_project_1920/LoginCheckActivity.java
@@ -0,0 +1,16 @@
+package com.example.iiatimd_project_1920;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LoginCheckActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_logincheck);
+
+
+    }
+}
